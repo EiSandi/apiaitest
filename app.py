e45a256c72f56f21bc256f73652b1c02e9f47cd5
EiSandi <eisandihtaywin95@gmail.com>
2017-04-17 14:32:28 -0500

    test weather+ movie

--- app.py
+++ app.py
@@ -154,24 +154,71 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "movietest":
-        return {}
-    baseurl = "https://api.themoviedb.org/3/discover/movie?primary_release_year=2016&sort_by=vote_average.desc&api_key=9b13737dd0119d542aabe1bf7bda84fc&format=json"
-   
-    yql_url = baseurl + "&format=json"
+    if req.get("result").get("action") == "movietest":
+        baseurl = "https://api.themoviedb.org/3/discover/movie?primary_release_year=2016&sort_by=vote_average.desc&api_key=9b13737dd0119d542aabe1bf7bda84fc&format=json"
+        yql_url = baseurl + "&format=json"
+    else:
+        baseurl = "https://query.yahooapis.com/v1/public/yql?"
+        yql_query = makeYqlQuery(req)
+        print('yql_query',yql_query)
+        if yql_query is None:
+            return {}
+        yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+
     result = urlopen(yql_url).read()
     data = json.loads(result)
     print('json.loads(result)')
     print(data)
 
-    res = makeWebhookResult(data)
+    res = makeWebhookResult(data,req)
     return res
 
 
-def makeWebhookResult(data):    
-    speech= "Movies:"
-    for movies in data['results']:
-        speech  = speech + "," + str(movies.get('title'))
+ def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
+
+def makeWebhookResult(data,req): 
+    if req.get("result").get("action") == "movietest":
+        speech= "Movies:"
+        for movies in data['results']:
+            speech  = speech + "," + str(movies.get('title'))
+            print("Response:")
+            print(speech)
+    else:
+        query = data.get('query')
+        if query is None:
+            return {}
+
+        result = query.get('results')
+        if result is None:
+            return {}
+
+        channel = result.get('channel')
+        if channel is None:
+            return {}
+
+        item = channel.get('item')
+        location = channel.get('location')
+        units = channel.get('units')
+        if (location is None) or (item is None) or (units is None):
+            return {}
+
+        condition = item.get('condition')
+        if condition is None:
+            return {}
+
+        
+        speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+                 ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
         print("Response:")
         print(speech)
 
@@ -183,6 +230,7 @@ def makeWebhookResult(data):
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
     }
+
 
 
 if __name__ == '__main__':
