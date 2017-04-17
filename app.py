90c6d84c9e491cba2f5625195b74239d5a8be713
EiSandi <eisandihtaywin95@gmail.com>
2017-04-17 11:18:16 -0500

    heroku deploy

--- app.py
+++ app.py
@@ -29,8 +29,10 @@ def webhook():
     res = processRequest(req)
 
     res = json.dumps(res, indent=4)
+
     print("final result---")
-    print(res)
+    print(json.dumps(res, indent=4))
+    
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
